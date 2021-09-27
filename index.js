f815c9bb618fe87732a49d0ff9e5e9ca80f5e4a9
oguzzarci <oguz.zarci@fcbhealthreaktor.com>
2021-09-27 17:48:06 +0300

    jaeger

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
 
+var privateKey  = fs.readFileSync('ssl/server.key', 'utf8');
+var certificate = fs.readFileSync('ssl/server.crt', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
 const swaggerui = require('swagger-ui-express');
 swaggerJsonFile = require('./swagger.json');
 app.use('/docui', swaggerui.serve, swaggerui.setup(swaggerJsonFile));
@@ -23,5 +30,8 @@ app.use(express.json());
 app.use('/api',requestRoutes);
 
 
+var httpsServer = https.createServer(credentials, app);
+
 //Api 5000 portunu kullanacak.
 app.listen(5000, () => console.log('Server Up and runnig'));
+httpsServer.listen(8443);
--- routers/request.js
+++ routers/request.js
@@ -1,15 +1,66 @@
 //express'i kullanarak bir router tanımlıyoruz.
 const router = require("express").Router();
+const opentracing = require('opentracing')
+const {initTracer} = require('jaeger-client')
 
 //router'i kullanarak get veya post ile gelecek istekleri karşılayacağız.
+
+
+//set up our tracer
+const config = {
+    serviceName: 'shipping',
+    reporter: {
+      logSpans: true,
+      collectorEndpoint: 'http://10.100.78.92:14268/api/traces',
+    },
+    sampler: {
+      type: 'const',
+      param: 1
+    }
+  };
+  const options = {
+    tags: {
+      'shipping.version': '3.1.2'
+    }
+  };
+
+
+const tracer = initTracer(config, options);
+
 
 // /getData url ile get işlemi yapıldığında.
 router.get('/getdata',async(req,res)=> {
 
-    res.json({
-        message:"Hello teknodice.com!"
+    const parentSpan = tracer.extract(opentracing.FORMAT_HTTP_HEADERS, req.headers)
+
+   // res.json({
+   //     message:"Hello teknodice.com!"
+   // })
+
+    const span = tracer.startSpan("fetching shipping info from USPS", {
+        childOf: parentSpan,
+        tags: {
+            [opentracing.Tags.COMPONENT]: "shipping"
+        }
     })
+
+    if (Math.random() > 0.3) {
+      res.json({
+        status: "delivered"
+      })
+    } else {
+      span.setTag(opentracing.Tags.ERROR, true)
+      res.status(503).send("External Shipping API unavailable")
+    }
+
+    span.finish()
 })
+
+
+
+
+
+
 
 // req : api'e gönderdiğimiz veri
 // res : gelen isteğe geri gönderidiğimiz cevap.
